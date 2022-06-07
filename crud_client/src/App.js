83688b0713ccdbb8520acf08afdc60e47fdc9223
Patech_Patrice <patricedrayton86@gmail.com>
2022-06-07 16:15:22 -0400

    integrated auth flow from react-client auth

--- crud_client/src/App.js
+++ crud_client/src/App.js
@@ -5,56 +5,49 @@ import Book from './components/book/book.js';
 import Home from './components/home/home.js';
 import UpdateBook from './components/update-book/update-book.js';
 import NewBook from './components/new-book/new-book.js';
-import Movies from './routes/movies/movies.component.js'
-import Movie from './routes/movie/movie.component.js'
-import NewMovie from './routes/new-movie/new-movie.component.js';
-import { useState } from 'react';
+import AuthContext from './store/auth-context';
+import Layout from './components/Layout/Layout';
+import UserProfile from './components/Profile/UserProfile';
+import AuthPage from './pages/AuthPage';
+import HomePage from './pages/HomePage';
+
+import { useState, useContext } from 'react';
 
 
-
-
-// const Home = () => {
-//   return (
-//     <div>
-//       This is the home page
-//       </div>
-//   );
-// }
-
-// const Movies = () => {
-//   return (
-//     <div>
-//       This is the movies  page
-//       </div>
-//   );
-// }
 
 const App = () => {
 
-
+const authCtx = useContext(AuthContext);
 
   return (
     <div className="App">
-      <h2>Welcome to the Book and Movie Logger App</h2>
-      <Link to="/books">View Books</Link> <Link to="movies">View Movies</Link>
-      {/* <Movies movies={movies} /> */}
-
- 
-   
-
-            <Routes>
-                <Route path='/' element={<Home/>} /> 
+     
+   <Layout>
+       <Routes>
+             <Route path='/' element={<HomePage/>} /> 
+              {!authCtx.isLoggedIn && (
+              <Route path="/auth" element={<AuthPage />} /> 
+              )}
+              { authCtx.isLoggedIn && (
+               <Route path="/profile" element={<UserProfile />} />
+              )}
+              { authCtx.isLoggedIn && (
+                <Route path="/books" element={<Books />} />
+              )}
+           
                 <Route path='/books/new' element={<NewBook/>} />
                 <Route path='/books/update/:id' element={<UpdateBook />} />
-                <Route path="/books" element={<Books />} />  
-                <Route path="movies" element={<Movies />} /> 
-                 <Route path="movies/:id" element={<Movie />} /> 
-                 <Route path='/movies/new' element={<NewMovie/>} />  
                 <Route path="/books/:id" element={<Book />} />
-                <Route path="/books/:id" element={<Navigate to='/books' />} />
-            </Routes>
+                <Route path="*" element={<Navigate to="/" replace />}
+    />
+          </Routes>
+      </Layout>
     </div>
   );
 }
 
 export default App;
+
+
+
+
---
+++ crud_client/src/components/Layout/MainNavigation.js
@@ -0,0 +1,67 @@
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import AuthContext from '../../store/auth-context';
+
+import classes from './MainNavigation.module.css';
+
+const MainNavigation = () => {
+
+ const authCtx = useContext(AuthContext);
+
+ const logoutHandler = () => {
+    authCtx.logout();
+    
+ };
+
+ const isLoggedIn = authCtx.isLoggedIn;
+  return (
+    <header className={classes.header}>
+      <Link to='/'>
+        <div className={classes.logo}>Book CRUD App</div>
+      </Link>
+      <nav>
+        <ul>
+            {!isLoggedIn && ( 
+          <li>
+            <Link to='/auth'>Login</Link>
+          </li>
+            )}
+
+            {isLoggedIn && (
+              <li>
+                <Link to='/profile'>Profile</Link>
+              </li>
+           )}
+               {isLoggedIn && (
+              <li>
+                <Link to='/books'>Books</Link>
+              </li>
+           )}
+                 {/* {isLoggedIn && (
+              <li>
+                <Link to='/books/new'>New Book</Link>
+              </li>
+           )} */}
+                 {/* {isLoggedIn && (
+              <li>
+                <Link to='/books/update/:id'>Update Book</Link>
+              </li>
+           )} */}
+                 {/* {isLoggedIn && (
+              <li>
+                <Link to='/books/:id'>Book ID Page</Link>
+              </li>
+           )} */}
+
+          {isLoggedIn && (
+          <li>
+            <button onClick={logoutHandler}>Logout</button>
+          </li>
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;

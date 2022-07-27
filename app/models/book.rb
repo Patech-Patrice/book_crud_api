cf1890c6144da8d83004137a2a319031c2ffbf5c
Patech_Patrice <patricedrayton86@gmail.com>
2022-07-27 09:43:55 -0400

    added back app folder

---
+++ app/models/book.rb
@@ -0,0 +1,16 @@
+class Book < ApplicationRecord
+    validates :title, presence: true
+
+    validates :body, presence: true
+   
+    validates :genre, presence: true
+
+    validates :author, presence: true
+
+    validates :image_url, presence: true
+
+ 
+
+ 
+
+end

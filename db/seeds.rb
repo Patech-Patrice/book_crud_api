4bed4a3e8489ff9b259fa8ab107a3128b714e1c2
Patech_Patrice <patricedrayton86@gmail.com>
2022-07-27 09:57:51 -0400

    added back deleted files

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+Book.create({title:'Harry Potter', body:'Boy Wizard', genre:'Fantasy Fiction', image_url:'https://images-na.ssl-images-amazon.com/images/I/81iqZ2HHD-L.jpg',author: 'J.K. Rowling'})
